774475403ce1581d1e8a2fe224ce85c496ebbe06
k0-R0 <bmenonprayush@gmail.com>
2026-09-06 19:06:56 +0530

    Add server read handlers

---
+++ src/server/server_utils.c
@@ -0,0 +1,21 @@
+#include "commons/commons.h"
+#include "commons/logs.h"
+
+Status recv_file_data(int fd, packet *pkt, int sock_fd,
+                      struct sockaddr_in *client_addr) {
+    ssize_t bytes_written;
+    pkt->block_num++;
+    pkt->opcode = DATA;
+    socklen_t len = sizeof(*client_addr);
+    if (recvfrom(sock_fd, pkt, sizeof(*pkt), 0, (struct sockaddr *)client_addr,
+                 &len) == -1) {
+        ERROR_SERVER_CONNECT();
+        return FAILURE;
+    }
+    if ((bytes_written = write(fd, pkt->data, sizeof(pkt->data))) < 0) {
+        ERROR_FILE_BLOCK_READ_FAILED(pkt->block_num);
+        perror(NULL);
+        return FAILURE;
+    }
+    return SUCCESS;
+}
---
+++ src/server/server_utils.h
@@ -0,0 +1,7 @@
+#ifndef SERVER_UTILS
+#define SERVER_UTILS
+#include "commons/commons.h"
+
+Status recv_file_data(int fd, packet *pkt, int sock_fd,
+                      struct sockaddr_in *server_addr);
+#endif
